9e44caf1aa9916d10bfbeb002e459c6af396e46d
SonNgo <chronos12022002@gmail.com>
2025-02-02 19:29:47 +0700

    build docker

--- src/main/java/com/example/team_work_management/config/FirebaseConfig.java
+++ src/main/java/com/example/team_work_management/config/FirebaseConfig.java
@@ -3,9 +3,9 @@ package com.example.team_work_management.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,7 +13,7 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
-    @Bean
+    @PostConstruct
     public void initFirebase() throws IOException{
         InputStream serviceAccount = getClass().getClassLoader()
                 .getResourceAsStream("static/datn-5ae48-firebase-adminsdk-dp31c-13d03bec68.json");
